0fd4ff4fdc75cdd3bae7731684eee3efc5211c2d
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-09-04 15:54:02 +0800

    rest.api

--- car_price/settings.py
+++ car_price/settings.py
@@ -56,8 +56,11 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
-
+    ],
+    'DEFAULT_PAGINATION_CLASS': [
+        'rest_framework.pagination.PageNumberPagination'
+    ],
+    'PAGE_SIZE': 50,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
--- dataset/views.py
+++ dataset/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework.pagination import LimitOffsetPagination
 from .models import CarDataSet
 from .serializer import Serializer
 
 # Create your views here.
+
+class Limit(LimitOffsetPagination):
+    default_limit = 10
+    max_limit = 100
 
 class CarViewSet(APIView):
     serializer_class = Serializer
 
     def get(self,request):
         car_dataset = CarDataSet.objects.all()
-        serializer = Serializer(car_dataset, many=True)
-        return Response(serializer.data)
+        paginator = Limit()
+        paginated_cars = paginator.paginate_queryset(car_dataset, request, view=self)
+        serializer = self.serializer_class(paginated_cars, many=True)
+        return paginator.get_paginated_response(serializer.data)
