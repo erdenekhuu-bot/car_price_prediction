d736c066915a864d06390bffd2fc097e164e0a70
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-09-04 16:23:35 +0800

    list.rest.api

--- home_page/src/App.tsx
+++ home_page/src/App.tsx
@@ -1,54 +1,75 @@
-import { Layout, Menu, theme } from "antd";
+import React, { useState, useId } from "react";
+import { Layout, theme, Button, Table, ConfigProvider } from "antd";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const { Header, Sider, Content } = Layout;
 
 const App: React.FC = () => {
-
+  const [collapsed, setCollapsed] = useState(true);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const id = useId();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["list", id],
+    queryFn: async () => {
+      const res = await axios.get("http://127.0.0.1:8000/api/");
+      return res.data;
+    },
+  });
+  console.log("data", data);
 
   return (
-    <Layout className="!h-screen !flex !flex-row-reverse">
-      <Sider trigger={null} width={500} collapsible>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#00b96b",
+          colorBgContainer,
+          borderRadiusLG,
+        },
+      }}
+    >
+      <Layout className="!h-screen !flex !flex-row-reverse">
+        <Sider trigger={null} width={500} collapsible collapsed={collapsed} />
 
-              label: "nav 1",
-            },
-            {
-              key: "2",
-
-              label: "nav 2",
-            },
-            {
-              key: "3",
-
-              label: "nav 3",
-            },
-          ]}
-        />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }}>1</Header>
-        <Content
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-            background: colorBgContainer,
-            borderRadius: borderRadiusLG,
-          }}
-        >
-          Content
-        </Content>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }}>
+            1
+          </Header>
+          <Content
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <Button onClick={() => setCollapsed(!collapsed)}>click</Button>
+            <Table
+              loading={isLoading}
+              dataSource={data?.results || []}
+              columns={[
+                {
+                  title: "ID",
+                  dataIndex: "id",
+                  key: "id",
+                },
+                {
+                  title: "price",
+                  dataIndex: "price",
+                  key: "price",
+                },
+              ]}
+              bordered
+              rowKey={"id"}
+            />
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
